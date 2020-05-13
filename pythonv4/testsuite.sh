26a76b264e8792a8bc233898f1ef7711ebdb853f
funderburkjim <funderburkjim@gmail.com>
2020-05-12 20:26:15 -0400

    TestSandiCoutv1:  revise compound sandhi standard so passed by python4 version of ScharfSandhi.  #6

--- pythonv4/testsuite.sh
+++ pythonv4/testsuite.sh
@@ -1,4 +1,4 @@
 python ScharfSandhiTest.py E ../testfiles/TestIn.txt ../testfiles/TestOut.txt
 python ScharfSandhiTest.py E1 ../testfiles/Testnn.txt ../testfiles/TestnnOut.txt
-python ScharfSandhiTest.py C ../testfiles/TestSandhiC.txt ../testfiles/TestSandhiCOut.txt
+python ScharfSandhiTest.py C ../testfiles/TestSandhiC.txt ../testfiles/TestSandhiCOutv1.txt
 python ScharfSandhiTest.py C ../testfiles/PascalSandhiTest3-nl.txt ../testfiles/PascalSandhiTest3S-nl.txt
